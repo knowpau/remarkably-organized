b9bbcca83ba4e524ffbaa643dbab8b6ae6679eee
knowpau <paulbknowles@gmail.com>
2026-03-05 10:48:34 -0600

    Remove Buy Me a Coffee widget
    Removed third party coffee donation widget

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,11 @@
 <script lang="ts">
-	import { dev } from '$app/environment';
 	import '$lib/styles/global.scss';
 </script>
 
 <svelte:head>
-	<title>Remarkably Organized</title>
-	{#if !dev}
-		<script
-			data-name="BMC-Widget"
-			data-cfasync="false"
-			src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-			data-id="brianschwabauer"
-			data-description="Support me on Buy me a coffee!"
-			data-message="Hi! If you like my work, consider buying me a coffee! I'd appreciate it!"
-			data-color="#5F7FFF"
-			data-position="left"
-			data-x_margin="12"
-			data-y_margin="12"></script>
-	{/if}
+	<title>Remarkably Organized Planner</title>
 </svelte:head>
+
 <slot />
 
 <style lang="scss">
